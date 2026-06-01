7341487833891ec023b7c2bdaead7da296ef681d
safacts <safacts001@gmail.com>
2026-06-02 04:48:23 +0530

    Fix ByteData type error in quotation_screen.dart

--- lib/quotation_screen.dart
+++ lib/quotation_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
