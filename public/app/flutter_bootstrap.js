5fe09d149ccc9eb4ac8a777af8e25e50a2280078
aadisheshu <safacts001@gmail.com>
2026-08-05 09:48:10 +0530

    fix: use Vitharn logo as default app icon (in-app fallback + PWA install icons) instead of Flutter logo

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4200785986" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "13227331" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
