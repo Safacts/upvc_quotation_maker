a8659c75ea3dacf413f9889c734cc4e8ff1215b5
aadisheshu <safacts001@gmail.com>
2026-08-28 20:22:12 +0530

    Forward dynamic client metadata to SEO routes

--- app/kprupvc/page.tsx
+++ app/kprupvc/page.tsx
@@ -1,6 +1,8 @@
-import MarketPageRoute, { generateMetadata } from "../[slug]/page";
+import MarketPageRoute, { generateMetadata as generateClientMetadata } from "../[slug]/page";
 
-export { generateMetadata };
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
+  return generateClientMetadata({ params });
+}
 
 export default function KprupvcPage() {
   return MarketPageRoute({ params: Promise.resolve({ slug: "kprupvc" }) });
--- app/venkateshwara/page.tsx
+++ app/venkateshwara/page.tsx
@@ -1,6 +1,8 @@
-import MarketPageRoute, { generateMetadata } from "../[slug]/page";
+import MarketPageRoute, { generateMetadata as generateClientMetadata } from "../[slug]/page";
 
-export { generateMetadata };
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
+  return generateClientMetadata({ params });
+}
 
 export default function VenkateshwaraPage() {
   return MarketPageRoute({ params: Promise.resolve({ slug: "venkateshwara" }) });
